aa9ec660ab104bf5c251efa10169765bac9af86d
Deni Supriyatna <denisupriyatna01@gmail.com>
2025-09-30 17:01:19 +0700

    update 30-09-2025

--- src/app/form-todo/page.jsx
+++ src/app/form-todo/page.jsx
@@ -1,15 +1,40 @@
-export default function FormTodo(params) {
-    function search(formData) {
-        const query = formData.getQuery("query");
-        alert(`you search form '${query}'`);
-    }
+"use client";
+
+import { useState, useEffect } from "react";
+
+import Layout from "@/components/layout";
+import Form from "next/form";
+
+export default function FormTodo() {
+    useEffect(() => {
+        console.log("render");
+    }, []);
+
+    const [queryValue, setQueryValue] = useState("");
+    const handelInputQueryChange = (event) => {
+        setQueryValue(event.target.value);
+    };
 
     return (
-        <>
-            <form action={search}>
-                <input type="text" name="query" />
+        <div className="container">
+            <Form id="form_query" action="/search">
+                <div className="w-full">
+                    <label htmlFor="query">Query</label>
+                    <input
+                        type="text"
+                        value={queryValue}
+                        onChange={handelInputQueryChange}
+                        placeholder="set query"
+                        className="bg-gray-50 text-gray-900 border-gray-300 text-sm rounded-sm"
+                        autoFocus
+                    />
+                </div>
                 <button type="submit">Search</button>
-            </form>
-        </>
+            </Form>
+
+            <div className="my-3">
+                <h1 className="text-lg font-bold text-white">{queryValue}</h1>
+            </div>
+        </div>
     );
 }
